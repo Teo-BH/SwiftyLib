15c0944f856c73a46f2196384f15f02c6336b539
Teobaldo Moura <tmoura@ciandt.com>
2019-03-08 11:01:05 -0300

    Add div function

--- SwiftyTeo/SwiftyTeo.swift
+++ SwiftyTeo/SwiftyTeo.swift
@@ -23,5 +23,8 @@ public final class SwiftyLib {
     public func mul(a: Int, b: Int) -> Int {
         return a * b
     }
-
+    
+    public func div(a: Int, b: Int) -> Int {
+        return a * b
+    }
 }
